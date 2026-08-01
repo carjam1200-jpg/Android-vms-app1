4e66376f2c5c391aa30492152bf79a241f4fdf1a
carjam1200-jpg <carjam1200@gmail.com>
2026-08-01 12:14:14 -0400

    Add VM list UI to main activity

--- app/src/main/java/com/carjam/androidvms/MainActivity.kt
+++ app/src/main/java/com/carjam/androidvms/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.carjam.androidvms
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(android.R.layout.simple_list_item_1)
+
+        val vmList = ListView(this)
+        val vms = arrayOf(
+            "Android-x86 7.1",
+            "Android-x86 9",
+            "Ubuntu VM",
+            "Custom Linux VM"
+        )
+
+        vmList.adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            vms
+        )
+
+        setContentView(vmList)
     }
 }
